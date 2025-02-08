336187835cacb81a8ac2c26a599c01a8eebc1522
Konrad <vanya.konrad@gmail.com>
2025-02-08 12:45:38 +0100

    create entities

---
+++ Data/Entities/ToDoItemEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace ToDoApi.Data.Entities;
+
+public class ToDoItemEntity : BaseEntity
+{
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public bool? IsCompleted { get; set; }
+    public DateTime CreateItem { get; set; } = DateTime.UtcNow;
+    public DateTime? Deadline { get; set; }
+
+    public PriorityLevel Priority { get; set; } = PriorityLevel.Low;
+
+    public Guid UserId { get; set; }
+    public UserEntity User { get; set; }
+}
---
+++ Data/Entities/UserEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoApi.Data.Entities;
+
+public class UserEntity : BaseEntity
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+
+    public List<ToDoItemEntity> TodoItems { get; set; }
+}
