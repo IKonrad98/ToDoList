bb910dfb399dcca6c3b3f680dd1971bbdba0e2af
Konrad <vanya.konrad@gmail.com>
2025-02-09 15:49:55 +0100

    add DI

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoApi.Data;
+using ToDoApi.DataAccess;
+using ToDoApi.DataAccess.RepoInterfaces;
+using ToDoApi.Infrastructure;
 using ToDoApi.Mappings;
+using ToDoApi.Services;
+using ToDoApi.Services.ServicesInterface;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ToDoApiDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+var connection = string.Empty;
+connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<DbContext, ToDoApiDbContext>(options =>
+    options.UseNpgsql(connection));
+
+builder.Services.AddTransient<IToDoItemRepo, ToDoItemRepo>();
+builder.Services.AddTransient<IToDoItemService, ToDoItemService>();
+builder.Services.AddTransient<IUserRepo, UserRepo>();
+builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddTransient<IPasswordRepo, PasswordRepo>();
+builder.Services.AddTransient<IPasswordEncryptionHelper, PasswordEncryptionHelper>();
 
 builder.Services.AddAutoMapper(typeof(ToDoApiMappingProfile));
 
