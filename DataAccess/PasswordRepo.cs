73c21a97fc40e1934517b18522736b318b825f03
Konrad <vanya.konrad@gmail.com>
2025-02-15 09:37:39 +0100

    add user repo

---
+++ DataAccess/PasswordRepo.cs
@@ -0,0 +1,14 @@
+﻿using ToDoApi.Data;
+using ToDoApi.Data.Entities;
+using ToDoApi.DataAccess.GenericRepo;
+using ToDoApi.DataAccess.RepoInterfaces;
+
+namespace ToDoApi.DataAccess;
+
+public class PasswordRepo : GenericRepo<PasswordEntity>, IPasswordRepo
+{
+    public PasswordRepo(ToDoApiDbContext context)
+        : base(context)
+    {
+    }
+}
---
+++ DataAccess/UserRepo.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApi.Data;
+using ToDoApi.Data.Entities;
+using ToDoApi.DataAccess.GenericRepo;
+using ToDoApi.DataAccess.RepoInterfaces;
+
+namespace ToDoApi.DataAccess;
+
+public class UserRepo : GenericRepo<UserEntity>, IUserRepo
+{
+    public UserRepo(ToDoApiDbContext context)
+        : base(context)
+    {
+    }
+
+    public async Task<UserEntity> GetNameAsync(string login, CancellationToken cancellationToken)
+    {
+        var entity = await _dbSet
+            .FirstOrDefaultAsync(u => u.UserName == login, cancellationToken);
+
+        return entity;
+    }
+
+    public async Task<UserEntity> GetByEmailAsync(string email, CancellationToken cancellationToken)
+    {
+        var entity = await _dbSet
+            .FirstOrDefaultAsync(u => u.Email == email, cancellationToken);
+
+        return entity;
+    }
+}
