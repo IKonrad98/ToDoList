5e21571cdb3672c2a00f4300a4b0b5fc4b7cc659
Konrad <vanya.konrad@gmail.com>
2025-02-18 17:41:59 +0100

    fix model user

--- Data/Models/CreateUserModel.cs
+++ Data/Models/CreateUserModel.cs
@@ -5,5 +5,4 @@ public class CreateUserModel
     public string UserName { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public DateTime CreateUser { get; set; }
 }
--- Data/Models/UserModel.cs
+++ Data/Models/UserModel.cs
@@ -5,4 +5,5 @@ public class UserModel
     public Guid Id { get; set; }
     public string UserName { get; set; }
     public string Email { get; set; }
+    public DateTime CreateUser { get; set; }
 }
